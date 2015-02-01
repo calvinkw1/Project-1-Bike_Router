122984ba79b809f325c4b3fbc07c02e0c2be112c
Calvin Wang <calvinkw1@gmail.com>
2015-02-01 13:09:09 -0800

    markers added on click event

--- app.js
+++ app.js
@@ -17,11 +17,16 @@
     mapLoaded = true;
     $(".addyForm").show();
     $(".getStarted").hide();
-  }
+    google.maps.event.addListener(document.getElementsByClassName("addyForm"), "submit", submitAddyBox());
+    google.maps.event.addListener(map, 'click', function(event) {
+      clickAddMarker(event.latLng);
+      console.log(event.latLng);
+  });
+
+}
 // the map init function will not work within a docready function
   google.maps.event.addDomListener(document.getElementById("getStartedBtn"), "click", initialize);
 
-// var startAddy = new google.maps.LatLng(lat, lng);
 
 function addMarker() {
   var myLatlng = new google.maps.LatLng(lat, lng);
@@ -52,47 +57,33 @@ function addMarker() {
   }
 }
 
-function clickAddMarker() {
-  if (mapLoaded === true) {
-    var clickLatLng = new google.maps.LatLng(lat, lng);
-    var mapOptions = {
-      zoom: 15,
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-    var marker = new google.maps.Marker({
-      draggable: true,
-      animation: google.maps.Animation.DROP,
-      position: clickLatLng,
-      title: "Start"
-    });
-    marker.setMap(map);
-  }
+function clickAddMarker(location) {
+  var marker = new google.maps.Marker({
+      position: location,
+      map: map
+  });
 }
-
-google.maps.event.addDomListener(document.getElementById("map-canvas"), "click", clickAddMarker);
 
 var lat,
     lng;
 
 // this function will retrieve the lattitude/longitude of any address that is entered into the text box upon "submit" and place a marker on the location
-$("form").on("submit", function(e) {
-  var address = $("#addyBox").val();
-  var url = encodeURI("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyDE6F79FbnrSc9hZlurECTyBJoEyHCj-Nc"); // this encodes the URL to account for spaces
-  // e.preventDefault();
-  // getJSON function below to retrieve the lat/lng from google's geocode api
-  $.getJSON(url, function(data) {
-    console.log(data);
-    var privLat = data.results[0].geometry.location.lat; // json result stored in variable
-    var privLng = data.results[0].geometry.location.lng; // json result stored in variable
-    lat = privLat; // storing private variables in public variables
-    lng = privLng;
-    console.log(lat + ", " + lng);
+function submitAddyBox() {
+  $("form").on("submit", function(e) {
+    var address = $("#addyBox").val();
+    var url = encodeURI("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyDE6F79FbnrSc9hZlurECTyBJoEyHCj-Nc"); // this encodes the URL to account for spaces
+    e.preventDefault();
+    // getJSON function below to retrieve the lat/lng from google's geocode api
+    $.getJSON(url, function(data) {
+      console.log(data);
+      var privLat = data.results[0].geometry.location.lat; // json result stored in variable
+      var privLng = data.results[0].geometry.location.lng; // json result stored in variable
+      lat = privLat; // storing private variables in public variables
+      lng = privLng;
+      console.log(lat + ", " + lng);
+    });
+    addMarker(); // calling function to drop marker on map
+    // $("#addyBox").val("");
   });
-  addMarker(); // calling function to drop marker on map
-  // $("#addyBox").val("");
-});
-
-// google.maps.event.addListener(map, "click", function (e) { 
-//         document.form1.waypointLog.value = e.latLng.lat().toFixed(6) + ' |' + e.latLng.lng().toFixed(6); 
-// }); 
+}
 
