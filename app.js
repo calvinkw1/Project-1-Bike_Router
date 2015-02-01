27de097773df678f3927ca9d10ac16acb1f73490
Calvin Wang <calvinkw1@gmail.com>
2015-01-31 23:34:43 -0800

    styling changes to show/hide map and description

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // initialize google's map
-  var map;
+  var map,
+      mapLoaded = false;
   function initialize() {
     var mapOptions = {
       zoom: 12,
@@ -13,12 +14,14 @@
     };
     map = new google.maps.Map(document.getElementById("map-canvas"),
         mapOptions);
+    mapLoaded = true;
+    $(".addyForm").show();
+    $(".getStarted").hide();
   }
-
-  google.maps.event.addDomListener(window, "load", initialize);
 // the map init function will not work within a docready function
+  google.maps.event.addDomListener(document.getElementById("getStartedBtn"), "click", initialize);
 
-var startAddy = new google.maps.LatLng(lat, lng);
+// var startAddy = new google.maps.LatLng(lat, lng);
 
 function addMarker() {
   var myLatlng = new google.maps.LatLng(lat, lng);
@@ -35,18 +38,38 @@ function addMarker() {
 
   // To add the marker to the map, call setMap();
   marker.setMap(map);
-  google.maps.event.addListener(marker, 'click', toggleBounce);
-}
 
-function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
+  // Event listener for click on the marker to invoke bouncing animation on marker
+  google.maps.event.addListener(marker, 'click', toggleBounce);
+
+  // this toggles the bouncing animation for the marker when marker is clicked
+  function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
   }
 }
 
-// google.maps.event.addDomListener(document.getElementById("map-canvas"), "click", addMarker);
+function clickAddMarker() {
+  if (mapLoaded === true) {
+    var clickLatLng = new google.maps.LatLng(lat, lng);
+    var mapOptions = {
+      zoom: 15,
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    var marker = new google.maps.Marker({
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      position: clickLatLng,
+      title: "Start"
+    });
+    marker.setMap(map);
+  }
+}
+
+google.maps.event.addDomListener(document.getElementById("map-canvas"), "click", clickAddMarker);
 
 var lat,
     lng;
@@ -55,16 +78,21 @@ var lat,
 $("form").on("submit", function(e) {
   var address = $("#addyBox").val();
   var url = encodeURI("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyDE6F79FbnrSc9hZlurECTyBJoEyHCj-Nc"); // this encodes the URL to account for spaces
-  e.preventDefault();
+  // e.preventDefault();
   // getJSON function below to retrieve the lat/lng from google's geocode api
   $.getJSON(url, function(data) {
+    console.log(data);
     var privLat = data.results[0].geometry.location.lat; // json result stored in variable
     var privLng = data.results[0].geometry.location.lng; // json result stored in variable
     lat = privLat; // storing private variables in public variables
     lng = privLng;
     console.log(lat + ", " + lng);
   });
-  addMarker();
+  addMarker(); // calling function to drop marker on map
+  // $("#addyBox").val("");
 });
 
+// google.maps.event.addListener(map, "click", function (e) { 
+//         document.form1.waypointLog.value = e.latLng.lat().toFixed(6) + ' |' + e.latLng.lng().toFixed(6); 
+// }); 
 
