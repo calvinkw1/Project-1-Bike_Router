3ec571ab238f3f4601903e6c346ff6572f68553c
Calvin Wang <calvinkw1@gmail.com>
2015-02-03 20:45:25 -0800

    directions panel working, but won't render the route lines without refreshing the map

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 // initialize google's map
 var map,
     name,
-    newMarker,
+    placesMarker,
+    wyptMarker,
     infowindow,
+    startLat,
+    startLng,
+    start = startLat + startLng,
+    lat,
+    lng,
+    endLat,
+    endLng,
+    endDest = parseInt(endLat) + parseInt(endLng),
+    wayptsArray = [],
+    placesArray = [],
     mapLoaded = false;
+
+// the map init function will not work within a docready function
+google.maps.event.addDomListener(document.getElementById("getStartedBtn"), "click", initialize);
+
 function initialize() {
   var mapOptions = {
     zoom: 12,
@@ -23,15 +38,29 @@ function initialize() {
   google.maps.event.addListener(document.getElementsByClassName("addyForm"), "submit", submitAddyBox());
 
 }
-// the map init function will not work within a docready function
-google.maps.event.addDomListener(document.getElementById("getStartedBtn"), "click", initialize);
 
-
+// this function will retrieve the lattitude/longitude of any address that is entered into the text box upon "submit" and place a marker on the location
+function submitAddyBox() {
+  $("form").on("submit", function(e) {
+    var address = $("#addyBox").val();
+    var url = encodeURI("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyDE6F79FbnrSc9hZlurECTyBJoEyHCj-Nc"); // this encodes the URL to account for spaces
+    e.preventDefault();
+    // getJSON function below to retrieve the lat/lng from google's geocode api
+    $.getJSON(url, function(data) {
+      var privLat = data.results[0].geometry.location.lat; // json result stored in variable
+      var privLng = data.results[0].geometry.location.lng; // json result stored in variable
+      startLat = privLat; // storing private variables in public variables
+      startLng = privLng;
+      addMarker(); // calling function to drop marker on map
+    });
+    $("#addyBox").val(""); // clear text after submit
+  });
+}
 
 function addMarker() {
-  var myLatlng = new google.maps.LatLng(lat, lng);
+  var myLatlng = new google.maps.LatLng(startLat, startLng);
   var mapOptions = {
-    zoom: 17,
+    zoom: 15,
     center: myLatlng
   };
   var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
@@ -40,6 +69,7 @@ function addMarker() {
     animation: google.maps.Animation.DROP,
     position: myLatlng
   });
+
 
   // To add the marker to the map, call setMap();
   marker.setMap(map);
@@ -51,7 +81,6 @@ function addMarker() {
 
   google.maps.event.addListener(map, 'click', function(event) {
     clickAddMarker(event.latLng);
-    // console.log(event.latLng);
   });
 
   function initPlaces() {
@@ -59,34 +88,50 @@ function addMarker() {
     var request = {
       location: markerLocation,
       radius: 100,
-      types: ["restaurant"]
+      types: ["bar"]
     };
     var service = new google.maps.places.PlacesService(map);
     service.nearbySearch(request, markPlaces);
   }
 
   function markPlaces(results, status) {
+    $(".placesList").css("visibility", "visible");
+    $(".listItems").empty();
     if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (var x = 0; x < placesArray.length; x++) {
+        placesArray[x].setMap(null);
+      }
+      placesArray = [];
+
       for (var i = 0; i < results.length; i++) {
         var position = new google.maps.LatLng(results[i].geometry.location.k, results[i].geometry.location.D);
-        newMarker = new google.maps.Marker({
+        placesMarker = new google.maps.Marker({
           map: map,
           image: results[i].icon,
           title: results[i].name,
-          position: position
+          position: position,
+          draggable: false
         });
+        var placeTitle = results[i].name;
+        var placeImage = results[i].icon;
+        var placeRating = results[i].rating;
+        var placeVicinity = results[i].vicinity;
+
+        placesArray.push(placesMarker);
         infowindow = new google.maps.InfoWindow({
           content: results[i].name
         });
-        google.maps.event.addListener(newMarker, 'click', openInfoWindow);
+        google.maps.event.addListener(placesMarker, 'click', openInfoWindow);
+        $(".listItems").append("<h4>" + placeTitle + "</h4><p>Address: " + placeVicinity + "</p><p>Rating: " + placeRating + "</p><hr>");
       }
     }
   }
 
+  // opens info window of a places marker
   function openInfoWindow() {
     infowindow.setContent('<div style="color:black; width: 75px;">' + this.title + '</div>');
-    console.log("infowindow",infowindow);
-    console.log("newMarker",this);
+    // console.log("infowindow",infowindow);
+    // console.log("placesMarker",this);
     infowindow.open(map, this);
   }
     // map.setCenter(results[i].geometry.location);   this line isn't needed unless centering the map after grabbing all places
@@ -101,93 +146,63 @@ function addMarker() {
   }
 
   function clickAddMarker(location) {
-  var marker = new google.maps.Marker({
+    // console.log(location);
+    lat = location.k;
+    lng = location.D;
+    endLat = lat;
+    endLng = lng;
+    var wyptLatlng = new google.maps.LatLng(lat, lng);
+    var wyptMarker = new google.maps.Marker({
       draggable: true,
-      position: location,
+      position: wyptLatlng,
       map: map,
       animation: google.maps.Animation.DROP,
-  });
-  waypts.push(marker);
-  console.log(waypts);
-  google.maps.event.addListener(marker, "dblclick", initPlaces);
+    });
+    // wayptsArray.push(wyptLatlng);
+    wayptsArray.push({
+      location: location,
+      stopover: true
+    });
+
+    google.maps.event.addListener(wyptMarker, "dblclick", initPlaces);
+    initDirections();
+    calcRoute();
   }
 }
 
-var lat,
-    lng,
-    waypts = [];
-
-// this function will retrieve the lattitude/longitude of any address that is entered into the text box upon "submit" and place a marker on the location
-function submitAddyBox() {
-  $("form").on("submit", function(e) {
-    var address = $("#addyBox").val();
-    var url = encodeURI("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyDE6F79FbnrSc9hZlurECTyBJoEyHCj-Nc"); // this encodes the URL to account for spaces
-    e.preventDefault();
-    // getJSON function below to retrieve the lat/lng from google's geocode api
-    $.getJSON(url, function(data) {
-
-      var privLat = data.results[0].geometry.location.lat; // json result stored in variable
-      var privLng = data.results[0].geometry.location.lng; // json result stored in variable
-      lat = privLat; // storing private variables in public variables
-      lng = privLng;
-      console.log(lat + ", " + lng);
-      addMarker(); // calling function to drop marker on map
-    });
-    $("#addyBox").val(""); // clear text after submit
-  });
-}
-
-
-
-var directionsDisplay;
+var rendererOptions = {
+  draggable: true
+};
+var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
 var directionsService = new google.maps.DirectionsService();
 
 function initDirections() {
+  $(".directionsBox").css("visibility", "visible");
   directionsDisplay = new google.maps.DirectionsRenderer();
-  var startLoc = new google.maps.LatLng(lat, lng);
+  start = new google.maps.LatLng(startLat, startLng);
   var mapOptions = {
-    zoom: 6,
-    center: startLoc
+    zoom: 5,
+    center: start
   };
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  directionsDisplay.setMap(map);
+  // map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  // directionsDisplay.setMap(map);
+  directionsDisplay.setPanel(document.getElementById("directions-panel"));
+
 }
 
 function calcRoute() {
-  var start = document.getElementById('start').value;
-  var end = document.getElementById('end').value;
-  var checkboxArray = document.getElementById('waypoints');
-  for (var i = 0; i < checkboxArray.length; i++) {
-    if (checkboxArray.options[i].selected === true) {
-      waypts.push({
-          location:checkboxArray[i].value,
-          stopover:true});
-    }
-  }
 
   var request = {
-      origin: start,
-      destination: end,
-      waypoints: waypts,
-      optimizeWaypoints: true,
-      travelMode: google.maps.TravelMode.BICYCLING
+    origin: start,
+    waypoints: wayptsArray,
+    destination: new google.maps.LatLng(endLat, endLng),
+    travelMode: google.maps.TravelMode.BICYCLING
   };
-  directionsService.route(request, function(response, status) {
+  console.log(request);
+  directionsService.route(request, function(result, status) {
     if (status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(response);
-      var route = response.routes[0];
-      var summaryPanel = document.getElementsByClassName("directionsBox");
-      summaryPanel.innerHTML = '';
-      // For each route, display summary information.
-      for (var i = 0; i < route.legs.length; i++) {
-        var routeSegment = i + 1;
-        summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment + '</b><br>';
-        summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
-        summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-        summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
-      }
+      directionsDisplay.setDirections(result);
     }
+    console.log(result + ", " + status);
   });
 }
-
-google.maps.event.addDomListener(document.getElementsByClassName("addyForm"), "submit", initDirections);
