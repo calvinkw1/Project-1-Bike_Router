70ff6f117381905a74f46cb500a8ea9e7139d659
Calvin Wang <calvinkw1@gmail.com>
2017-03-18 19:57:52 -0700

    clean up gulpfile task

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'))
+    gulp.src('sass/**/*.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('./css/'))
 });
 
 //Watch task
